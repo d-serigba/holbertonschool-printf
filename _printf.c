b61c39474e2d1a710342b4b79c1facca0b1690cd
Dylan Serigba <12297@holbertonstudents.com>
2025-11-25 16:17:14 +0100

    Conversations specifiers added

--- _printf.c
+++ _printf.c
@@ -1,219 +1,36 @@
 #include "main.h"
 
 /**
- * print_char - affiche un caractère
- * @args: liste des arguments variables
+ * _printf - Produces output according to a format
+ * @format: Is a character string. The format string
+ * is composed of zero or more directives
  *
- * Return: nombre de caractères imprimés (toujours 1)
- */
-int print_char(va_list args)
-{
-    char c = va_arg(args, int);
-    
-    _putchar(c);
-    return (1);
-}
-
-/**
- * print_string - affiche une chaîne de caractères
- * @args: liste des arguments variables
- *
- * Return: nombre de caractères imprimés
- */
-int print_string(va_list args)
-{
-    char *str = va_arg(args, char *);
-    int count = 0;
-    
-    /* Si la chaîne est NULL, on affiche "(null)" */
-    if (str == NULL)
-        str = "(null)";
-    
-    /* On parcourt la chaîne caractère par caractère */
-    while (str[count])
-    {
-        _putchar(str[count]);
-        count++;
-    }
-    
-    return (count);
-}
-
-/**
- * print_percent - affiche le symbole %
- * @args: liste des arguments (non utilisée ici)
- *
- * Return: nombre de caractères imprimés (toujours 1)
- */
-int print_percent(va_list args)
-{
-    (void)args; /* On ignore l'argument */
-    
-    _putchar('%');
-    return (1);
-}
-
-/**
- * handle_format - gère les spécificateurs de format
- * @format: le caractère après le %
- * @args: liste des arguments variables
- *
- * Return: nombre de caractères imprimés, ou -1 si format invalide
- */
-int handle_format(char format, va_list args)
-{
-    if (format == 'c')
-        return (print_char(args));
-    else if (format == 's')
-        return (print_string(args));
-    else if (format == '%')
-        return (print_percent(args));
-    
-    /* Si le format n'est pas reconnu, on affiche % suivi du caractère */
-    _putchar('%');
-    _putchar(format);
-    return (2);
-}
-
-/**
- * _printf - produit une sortie selon un format
- * @format: chaîne de format contenant des directives
- *
- * Return: le nombre de caractères imprimés
- */
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0;
-    int count = 0;
-    
-    /* Si format est NULL, on retourne -1 */
-    if (format == NULL)
-        return (-1);
-    
-    /* On initialise la liste d'arguments variables */
-    va_start(args, format);
-    
-    /* On parcourt la chaîne format caractère par caractère */
-    while (format[i])
-    {
-        /* Si on trouve un %, on traite le spécificateur qui suit */
-        if (format[i] == '%')
-        {
-            i++; /* On passe au caractère après % */
-            
-            /* Si on arrive à la fin de la chaîne après %, on arrête */
-            if (format[i] == '\0')
-                break;
-            
-            /* On traite le spécificateur de format */
-            count += handle_format(format[i], args);
-        }
-        else
-        {
-            /* Sinon, on affiche simplement le caractère */
-            _putchar(format[i]);
-            count++;
-        }
-        i++;
-    }
-    
-    /* On nettoie la liste d'arguments */
-    va_end(args);
-    
-    return (count);
-}
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"u", unsigned_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{NULL, NULL}
+	};
 
-/**
- * print_number - Prints an integer
- * @n: The integer to print
- *
- * Return: Number of characters printed
- */
-int print_number(int n)
-{
-	int count = 0;
-	unsigned int num;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		count++;
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
-
-	if (num / 10)
-		count += print_number(num / 10);
-
-	_putchar((num % 10) + '0');
-	count++;
-
-	return (count);
-}
-
-/**
- * _printf - Custom printf function
- * @format: Format string containing text and format specifiers
- *
- * Return: Number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int i = 0, count = 0;
+	va_list arg_list;
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(args, format);
+	va_start(arg_list, format);
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-				return (-1);
-
-			if (format[i] == 'd' || format[i] == 'i')
-			{
-				count += print_number(va_arg(args, int));
-			}
-			else if (format[i] == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		i++;
-	}
-
-	va_end(args);
-	return (count);
-}
-
-/**
- * _putchar - Writes a character to stdout
- * @c: The character to print
- *
- * Return: On success 1, on error -1
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	printed_chars = recording_error(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
