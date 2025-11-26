c488ddc9da4a5a1ba532a3edccf948d8e3a57cb9
Dylan Serigba <12297@holbertonstudents.com>
2025-11-26 13:36:53 +0100

    Header Printf

--- main.h
+++ main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h> /* Pour va_list */
-#include <unistd.h> /* Pour _putchar/write */
-#include <limits.h> /* IMPORTANT: Pour INT_MIN */
-#include <stdlib.h> /* Utile pour la gestion des chaînes/nombres */
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h> /* Ajouté pour INT_MIN */
+#include <stdlib.h>
 
 /**
  * struct convert - Struct for conversion specifiers
- * @sym: The format specifier
- * @f: The function associated
+ * @sym: The format specifier (e.g., "c", "s", "%", "d", "i")
+ * @f: The function associated (pointeur de fonction)
  */
 typedef struct convert
 {
@@ -17,22 +17,46 @@ typedef struct convert
     int (*f)(va_list);
 } conver_t;
 
-/* Main printf function */
+/* ------------------------------------------------------------------ */
+/* Fonction Principale */
+/* ------------------------------------------------------------------ */
+
+/**
+ * _printf - Produit une sortie formatée vers stdout.
+ * @format: Chaîne de caractères contenant zéro ou plusieurs directives.
+ * Gère les spécificateurs de conversion suivants :
+ * - 'c': caractère simple
+ * - 's': chaîne de caractères
+ * - '%': caractère pourcentage
+ * - 'd' / 'i': entier signé (decimal et integer)
+ *
+ * NOTE: Cette implémentation ne gère pas:
+ * - Les caractères de drapeau (flags)
+ * - La largeur de champ (field width)
+ * - La précision (precision)
+ * - Les modificateurs de longueur (length modifiers)
+ *
+ * Return: Le nombre de caractères imprimés (excluant le null byte de fin de chaîne).
+ */
 int _printf(const char *format, ...);
 
-/* Parser */
+/* ------------------------------------------------------------------ */
+/* Fonctions de Parsing et de Décomposition */
+/* ------------------------------------------------------------------ */
+
+/* * parser - Fonction responsable de parcourir le format, 
+ * de chercher le spécificateur et d'appeler la bonne fonction.
+ */
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 
 /* ------------------------------------------------------------------ */
-/* Functions pour les Spécificateurs */
+/* Fonctions d'Impression des Spécificateurs */
 /* ------------------------------------------------------------------ */
 
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
-
-/* Nouveau: Gère %d et %i. C'est elle qui appelle print_number pour l'affichage. */
-int print_integer(va_list args); 
+int print_integer(va_list args);
 
 /* ------------------------------------------------------------------ */
 /* Fonctions Utilitaires (Helpers) */
@@ -40,7 +64,7 @@ int print_integer(va_list args);
 
 int _putchar(char c);
 
-/* Mise à jour: Change le type d'argument à long int pour gérer INT_MIN sans débordement. */
+/* Affiche un nombre (utilisée par print_integer, prend long int pour INT_MIN) */
 int print_number(long int n); 
 
-#endif
+#endif /* MAIN_H */
